446cf8c8d66e7568a4e503996ad94958bed5e00c
Qiao <qiaokuanghua@gmail.com>
2016-11-08 15:10:04 -0500

    feat(myapp): add myapp app [WIP]
    - Tester app for demonstration
    - Enable keyboard input
    
    TODO:
    - Msg data struct
    - Keyboard input interface

---
+++ apps/myapp/fsw/mission_inc/myapp_mission_cfg.h
@@ -0,0 +1,41 @@
+/*=======================================================================================
+** File Name:  myapp_mission_cfg.h
+**
+** Title:  Mission Configuration Header File for MYAPP Application
+**
+** $Author:    CSDC
+** $Revision: 1.1 $
+** $Date:      2016-11-08
+**
+** Purpose:  This header file contains declartions and definitions of all MYAPP's 
+**           mission-specific configurations.
+**
+** Modification History:
+**   Date | Author | Description
+**   ---------------------------
+**   2016-11-08 | CSDC | Build #: Code Started
+**
+**=====================================================================================*/
+    
+#ifndef _MYAPP_MISSION_CFG_H_
+#define _MYAPP_MISSION_CFG_H_
+
+/*
+** Include Files
+*/
+#include "cfe.h"
+
+
+
+/*
+** MYAPP Mission Configuration Parameter Definitions
+*/
+
+/* TODO:  Add mission configuration parameter definitions here, if necessary. */
+
+#endif /* _MYAPP_MISSION_CFG_H_ */
+
+/*=======================================================================================
+** End of file myapp_mission_cfg.h
+**=====================================================================================*/
+    
---
+++ apps/myapp/fsw/mission_inc/myapp_perfids.h
@@ -0,0 +1,33 @@
+/*=======================================================================================
+** File Name:  myapp_perfids.h
+**
+** Title:  Performance ID Header File for MYAPP Application
+**
+** $Author:    CSDC
+** $Revision: 1.1 $
+** $Date:      2016-11-08
+**
+** Purpose:  This header file contains declartions and definitions of all MYAPP's 
+**           Performance IDs.
+**
+** Modification History:
+**   Date | Author | Description
+**   ---------------------------
+**   2016-11-08 | CSDC | Build #: Code Started
+**
+**=====================================================================================*/
+    
+#ifndef _MYAPP_PERFIDS_H_
+#define _MYAPP_PERFIDS_H_
+
+#define MYAPP_MAIN_TASK_PERF_ID            50
+
+    
+
+
+#endif /* _MYAPP_PERFIDS_H_ */
+
+/*=======================================================================================
+** End of file myapp_perfids.h
+**=====================================================================================*/
+    
